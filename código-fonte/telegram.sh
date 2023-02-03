313456bfd5a4e33dbcda3c5d82d37e2d79af82ee
gabrielaggn <110793151+gabrielaggn@users.noreply.github.com>
2023-02-03 07:50:25 -0300

    Create telegram.sh

---
+++ código-fonte/telegram.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+TOKEN="5930363802:AAH1VMsE95atmqlp9Eblrzjpe_cMR-czR-8"
+ 
+USER=$1
+SUBJECT=$2
+MESSAGE=$3
+NL="
+"
+curl --silent -X POST --data-urlencode "chat_id=${USER}" --data-urlencode "text>
+ 
+exit 0
